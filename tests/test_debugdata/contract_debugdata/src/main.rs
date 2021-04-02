0373413719d26e978e20781061ae64e79075c999
dezuman <johannes.borg@hotmail.com>
2021-04-03 01:38:52 +0200

    Clean up testing smart contract

--- tests/test_debugdata/contract_debugdata/src/main.rs
+++ tests/test_debugdata/contract_debugdata/src/main.rs
@@ -11,30 +11,14 @@ extern crate panic_halt;
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
-    /*
-    let msg = "Hello Costack!";
-    __push_costack(msg.as_ptr(), msg.len());
+    __system_call(0x8000_0001, 0); // DebugDataFunctions::Available
+
+    __system_call(0x8000_0001, 1); // DebugDataFunctions::PushInputStack
     
+    let mut array: [u8; 500] = [0; 500];
+    let actual_size = __pop_costack(array.as_mut_ptr(), array.len());
+    __push_costack(array.as_ptr(), actual_size);
     
-    let msg = "Hello Costack trough logger!";
-    __push_costack(msg.as_ptr(), msg.len());
-    let count = 1 as u8;
-    let count_ptr: *const u8 = &count;
-    __push_costack(count_ptr, 1);
-    __system_call(4, 1);
-    */
-    
-    __system_call(0x8000_0001, 0);
-    
-    //let msg = "Hello Costack!";
-    //__push_costack(msg.as_ptr(), msg.len());
-    //__system_call(0x8000_0001, 2);
-    __system_call(0x8000_0001, 1);
-    
-    let mut array: [u8; 25] = [0; 25];
-    __pop_costack(array.as_mut_ptr(), array.len());
-    __push_costack(array.as_ptr(), array.len());
-    
-    __system_call(0x8000_0001, 2);
+    __system_call(0x8000_0001, 2); // DebugDataFunctions::AssertOutputStack
     __exit(5);
 }
