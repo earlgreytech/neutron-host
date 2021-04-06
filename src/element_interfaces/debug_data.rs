e52eb83e2d968b1e362d084a0ceb15840bdc532d
dezuman <johannes.borg@hotmail.com>
2021-04-06 16:45:50 +0200

    Do rustfmt on added elementAPI source file

--- src/element_interfaces/debug_data.rs
+++ src/element_interfaces/debug_data.rs
@@ -1,22 +1,22 @@
-use crate::codata::*;
-use crate::neutronerror::*;
-use crate::neutronerror::NeutronError::*;
-use neutron_common::RecoverableError;
 use crate::callsystem::*;
-use std::str;
+use crate::codata::*;
+use crate::neutronerror::NeutronError::*;
+use crate::neutronerror::*;
 use core::mem::transmute;
+use neutron_common::RecoverableError;
+use std::str;
 /*
 ## Debug Data Injector ElementAPI
 
-ID: 0x8000_0001 
+ID: 0x8000_0001
 
 This API is used in a testing environment to inject data into a test instance of the Neutron stack
 This file also contains some data structures that simplifies construction of this data
 
-Data structures: 
+Data structures:
 
 * mock_input_stack          - An initial input stack state that the testing smart contract can load
-* expected_output_stack     - An expected state for the output stack that the testing smart contract can asserted against, 
+* expected_output_stack     - An expected state for the output stack that the testing smart contract can asserted against,
                               along with debug information to make a failed assertion more informative than "bytes in stacks didn't match"
 
 functions:
@@ -27,58 +27,67 @@ functions:
 
 */
 
-// IDs >= 0x8000_0000 are meant for "internal" usage within the project, e.g. for unit/integration testing. 
+// IDs >= 0x8000_0000 are meant for "internal" usage within the project, e.g. for unit/integration testing.
 pub const DEBUG_DATA_FEATURE: u32 = 0x8000_0001;
 
 #[derive(FromPrimitive)]
-pub enum DebugDataFunctions{
+pub enum DebugDataFunctions {
     Available = 0, //reserved??
     PushInputStack = 1,
     AssertOutputStack = 2,
 }
 
 #[derive(Default)]
-pub struct DebugDataInjector{
+pub struct DebugDataInjector {
     pub mock_input_stack: DebugCoDataStack,
     pub expected_output_stack: DebugCoData,
 }
 
-impl ElementAPI for DebugDataInjector{
-    fn system_call(&mut self, _callsystem: &CallSystem, codata: &mut CoData, feature: u32, function: u32) -> Result<ElementResult, NeutronError>{
-        
+impl ElementAPI for DebugDataInjector {
+    fn system_call(
+        &mut self,
+        _callsystem: &CallSystem,
+        codata: &mut CoData,
+        feature: u32,
+        function: u32,
+    ) -> Result<ElementResult, NeutronError> {
         let f = num::FromPrimitive::from_u32(function);
-        if f.is_none(){
+        if f.is_none() {
             return Err(Recoverable(RecoverableError::InvalidSystemFunction));
         }
-        let f=f.unwrap();
-        
+        let f = f.unwrap();
+
         let result = match f {
             DebugDataFunctions::Available => {
                 println!("[DebugDataInjector] Called with feature: 0 (Available)");
                 Ok(())
-            }, 
+            }
             DebugDataFunctions::PushInputStack => {
                 println!("[DebugDataInjector] Called with feature: 1 (PushInputStack)");
 
                 codata.push_output_stack(&self.mock_input_stack.stack.as_slice());
                 Ok(())
-            }, 
+            }
             DebugDataFunctions::AssertOutputStack => {
                 println!("[DebugDataInjector] Called with feature: 2 (AssertOutputStack)");
-                
+
                 let mut exec_output_stack = codata.pop_input_stack().unwrap();
 
-                println!("    Popped {} elements from codata", exec_output_stack.len());
+                println!(
+                    "    Popped {} elements from codata",
+                    exec_output_stack.len()
+                );
 
-                self.expected_output_stack.assert_output_eq(&mut exec_output_stack);
+                self.expected_output_stack
+                    .assert_output_eq(&mut exec_output_stack);
 
                 Ok(())
-            }, 
+            }
         };
-        
-        if result.is_err(){
+
+        if result.is_err() {
             Err(result.unwrap_err())
-        }else{
+        } else {
             return Ok(ElementResult::Result(0));
         }
     }
@@ -91,84 +100,74 @@ pub struct DebugCoDataStack {
 }
 
 impl DebugCoDataStack {
-
     // The following functions are adapted from neutron-star/src/syscall.rs
-    
-    // Pushes a u64 value to the stack. 
-    pub fn push_u64(&mut self, value: u64){
-        const SIZE:usize = 8;
-        let t = unsafe{
-            transmute::<u64, [u8; SIZE]>(value)
-        };
+
+    // Pushes a u64 value to the stack.
+    pub fn push_u64(&mut self, value: u64) {
+        const SIZE: usize = 8;
+        let t = unsafe { transmute::<u64, [u8; SIZE]>(value) };
         for byte in &t {
             self.stack.push(*byte);
         }
     }
-    // Pushes a u32 value to the stack. 
-    pub fn push_u32(&mut self, value: u32){
-        const SIZE:usize = 4;
-        let t = unsafe{
-            transmute::<u32, [u8; SIZE]>(value)
-        };
+    // Pushes a u32 value to the stack.
+    pub fn push_u32(&mut self, value: u32) {
+        const SIZE: usize = 4;
+        let t = unsafe { transmute::<u32, [u8; SIZE]>(value) };
         for byte in &t {
             self.stack.push(*byte);
         }
     }
 
-    // Pushes a u16 value to the stack. 
-    pub fn push_u16(&mut self, value: u16){
-        const SIZE:usize = 2;
-        let t = unsafe{
-            transmute::<u16, [u8; SIZE]>(value)
-        };
+    // Pushes a u16 value to the stack.
+    pub fn push_u16(&mut self, value: u16) {
+        const SIZE: usize = 2;
+        let t = unsafe { transmute::<u16, [u8; SIZE]>(value) };
         for byte in &t {
             self.stack.push(*byte);
         }
     }
 
-    // Pushes a u8 value to the stack. 
-    pub fn push_u8(&mut self, value: u8){
-        const SIZE:usize = 1;
-        let t = unsafe{
-            transmute::<u8, [u8; SIZE]>(value)
-        };
+    // Pushes a u8 value to the stack.
+    pub fn push_u8(&mut self, value: u8) {
+        const SIZE: usize = 1;
+        let t = unsafe { transmute::<u8, [u8; SIZE]>(value) };
         for byte in &t {
             self.stack.push(*byte);
         }
     }
-    
+
     // Wrappers for the above functions to also handle signed integers
-    pub fn push_i64(&mut self, value: i64){
+    pub fn push_i64(&mut self, value: i64) {
         self.push_u64(value as u64);
     }
-    pub fn push_i32(&mut self, value: i32){
+    pub fn push_i32(&mut self, value: i32) {
         self.push_u32(value as u32);
     }
-    pub fn push_i16(&mut self, value: i16){
+    pub fn push_i16(&mut self, value: i16) {
         self.push_u16(value as u16);
     }
-    pub fn push_i8(&mut self, value: i8){
+    pub fn push_i8(&mut self, value: i8) {
         self.push_u8(value as u8);
     }
-    
-    /// Pushes a byte sequence to the stack. 
-    pub fn push_bytes(&mut self, value: &[u8]){
+
+    /// Pushes a byte sequence to the stack.
+    pub fn push_bytes(&mut self, value: &[u8]) {
         for byte in value {
             self.stack.push(*byte);
         }
     }
-    
+
     // These functions simply mirrors regular vector behavior for convenience
-    
-    pub fn pop(&mut self) -> Option<u8>{
+
+    pub fn pop(&mut self) -> Option<u8> {
         self.stack.pop()
     }
-    
-    pub fn len(&mut self) -> usize{
+
+    pub fn len(&mut self) -> usize {
         self.stack.len()
     }
 }
-
 
 // Used to easily construct an expected codata state, along with extra debugging information
 #[derive(Default)]
@@ -180,90 +179,95 @@ pub struct DebugCoData {
 }
 
 impl DebugCoData {
-    // Pushes a u64 value to the stack. 
-    pub fn push_u64(&mut self, value: u64, name: &str){
-        const SIZE:usize = 8;
+    // Pushes a u64 value to the stack.
+    pub fn push_u64(&mut self, value: u64, name: &str) {
+        const SIZE: usize = 8;
         self.output_stack.push_u64(value);
-        
+
         self.push_debug_data(name, "u64", SIZE);
     }
-    
-    // Pushes a u32 value to the stack. 
-    pub fn push_u32(&mut self, value: u32, name: &str){
-        const SIZE:usize = 4;
+
+    // Pushes a u32 value to the stack.
+    pub fn push_u32(&mut self, value: u32, name: &str) {
+        const SIZE: usize = 4;
         self.output_stack.push_u32(value);
-        
+
         self.push_debug_data(name, "u32", SIZE);
     }
 
-    // Pushes a u16 value to the stack. 
-    pub fn push_u16(&mut self, value: u16, name: &str){
-        const SIZE:usize = 2;
+    // Pushes a u16 value to the stack.
+    pub fn push_u16(&mut self, value: u16, name: &str) {
+        const SIZE: usize = 2;
         self.output_stack.push_u16(value);
-        
+
         self.push_debug_data(name, "u16", SIZE);
     }
 
-    // Pushes a u8 value to the stack. 
-    pub fn push_u8(&mut self, value: u8, name: &str){
-        const SIZE:usize = 1;
+    // Pushes a u8 value to the stack.
+    pub fn push_u8(&mut self, value: u8, name: &str) {
+        const SIZE: usize = 1;
         self.output_stack.push_u8(value);
-        
+
         self.push_debug_data(name, "u8", SIZE);
     }
-    
-    // Pushes a i64 value to the stack. 
-    pub fn push_i64(&mut self, value: i64, name: &str){
-        const SIZE:usize = 8;
+
+    // Pushes a i64 value to the stack.
+    pub fn push_i64(&mut self, value: i64, name: &str) {
+        const SIZE: usize = 8;
         self.output_stack.push_i64(value);
-        
+
         self.push_debug_data(name, "i64", SIZE);
     }
-    
-    // Pushes a i32 value to the stack. 
-    pub fn push_i32(&mut self, value: i32, name: &str){
-        const SIZE:usize = 4;
+
+    // Pushes a i32 value to the stack.
+    pub fn push_i32(&mut self, value: i32, name: &str) {
+        const SIZE: usize = 4;
         self.output_stack.push_i32(value);
-        
+
         self.push_debug_data(name, "i32", SIZE);
     }
 
-    // Pushes a i16 value to the stack. 
-    pub fn push_i16(&mut self, value: i16, name: &str){
-        const SIZE:usize = 2;
+    // Pushes a i16 value to the stack.
+    pub fn push_i16(&mut self, value: i16, name: &str) {
+        const SIZE: usize = 2;
         self.output_stack.push_i16(value);
-        
+
         self.push_debug_data(name, "i16", SIZE);
     }
 
-    // Pushes a i8 value to the stack. 
-    pub fn push_i8(&mut self, value: i8, name: &str){
-        const SIZE:usize = 1;
+    // Pushes a i8 value to the stack.
+    pub fn push_i8(&mut self, value: i8, name: &str) {
+        const SIZE: usize = 1;
         self.output_stack.push_i8(value);
-        
+
         self.push_debug_data(name, "i8", SIZE);
     }
-    
-    // Pushes a byte sequence to the stack. 
-    pub fn push_bytes(&mut self, value: &[u8], name: &str){
-        let SIZE:usize = value.len();
+
+    // Pushes a byte sequence to the stack.
+    pub fn push_bytes(&mut self, value: &[u8], name: &str) {
+        let SIZE: usize = value.len();
         self.output_stack.push_bytes(value);
-        
+
         self.push_debug_data(name, "byte-sequence", SIZE);
     }
-    
+
     // Checks stack
-    pub fn assert_output_eq(&mut self, stack_to_compare: &mut Vec<u8>){
+    pub fn assert_output_eq(&mut self, stack_to_compare: &mut Vec<u8>) {
         let expected_stack_length = self.output_stack.stack.len();
         let actual_stack_length = stack_to_compare.len();
         // If length isn't equal it means there's a mismatch of variable count and/or types
-        assert!(expected_stack_length == actual_stack_length, "\n\n[DebugCoData] assert_output_eq: Expected output stack size {}, was {}. \n\n", expected_stack_length, actual_stack_length);
-        
+        assert!(
+            expected_stack_length == actual_stack_length,
+            "\n\n[DebugCoData] assert_output_eq: Expected output stack size {}, was {}. \n\n",
+            expected_stack_length,
+            actual_stack_length
+        );
+
         // If we expected zero-length output and got zero-length output we are done
         if expected_stack_length == 0 {
             return ();
         }
-        
+
         while self.variable_sizes.len() > 0 {
             let name = self.variable_names.pop().unwrap();
             let data_type = self.variable_types.pop().unwrap();
@@ -271,12 +275,20 @@ impl DebugCoData {
             for _i in 0..size {
                 let byte_expected = self.output_stack.pop();
                 let byte_comparison = stack_to_compare.pop();
-                assert!(byte_expected == byte_comparison, "\n\n[DebugCoData] assert_output_eq: Wrong value found in {} named {}. \n\n", data_type, name);
+                assert!(
+                    byte_expected == byte_comparison,
+                    "\n\n[DebugCoData] assert_output_eq: Wrong value found in {} named {}. \n\n",
+                    data_type,
+                    name
+                );
             }
-            println!("[DebugCoData] assert_output_eq: Correct value found in {} named {}", data_type, name);
+            println!(
+                "[DebugCoData] assert_output_eq: Correct value found in {} named {}",
+                data_type, name
+            );
         }
     }
-    
+
     // Internal stuff
     fn push_debug_data(&mut self, name: &str, type_str: &str, size: usize) {
         self.variable_names.push(String::from(name));
