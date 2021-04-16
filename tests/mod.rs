1b0dea697abcdf4487d63e05336bbd16427d707d
dezuman <johannes.borg@hotmail.com>
2021-04-16 15:53:52 +0200

    Add initial tests for hypervisor comap operations

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,5 +1,6 @@
 mod harness;
 
 // Every subfolder with tests need to be defined as modules here, otherwise they won't be executed
+mod test_comap;
 mod test_debugdata;
 mod test_smoke;
---
+++ tests/test_comap/mod.rs
@@ -0,0 +1,129 @@
+use crate::harness::*;
+use crate::*;
+
+// These will throw lots of unused import warnings because some are only used in macros
+use neutron_host::callsystem::*;
+use neutron_host::codata::*;
+use neutron_host::db::MemoryGlobalState;
+use neutron_host::element_interfaces::debug_data::*;
+use neutron_host::element_interfaces::logging::StdoutLogger;
+use neutron_host::interface::*;
+use neutron_host::manager::*;
+use neutron_host::narm_hypervisor::*;
+use neutron_host::vmmanager::*;
+
+use std::cell::RefCell;
+use std::env;
+
+const DIR_NAME: &'static str = "test_comap";
+const CONTRACT_DIR_NAME_1: &'static str = "contract_stack_to_map";
+
+const CONTRACT_DIR_NAME_2: &'static str = "contract_map_to_stack";
+
+#[test]
+fn comap_push() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let data = "this is the data";
+
+    stack.push_str(key);
+    stack.push_str(data);
+
+    expected_map
+        .push_key(key.as_bytes(), data.as_bytes())
+        .unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_map: expected_map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME_1);
+    initiateAndRun!(harness);
+}
+
+#[test]
+#[should_panic]
+fn comap_push_negtest_wrong_key() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let wrong_key = "this is the WRONG key";
+    let data = "this is the data";
+
+    stack.push_str(key);
+    stack.push_str(data);
+
+    expected_map
+        .push_key(wrong_key.as_bytes(), data.as_bytes())
+        .unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_map: expected_map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME_1);
+    initiateAndRun!(harness);
+}
+
+#[test]
+#[should_panic]
+fn comap_push_negtest_wrong_data() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let data = "this is the data";
+    let wrong_data = "this is the WRONG data";
+
+    stack.push_str(key);
+    stack.push_str(data);
+
+    expected_map
+        .push_key(key.as_bytes(), wrong_data.as_bytes())
+        .unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_map: expected_map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME_1);
+    initiateAndRun!(harness);
+}
+
+#[test]
+fn comap_peek() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_stack = WrappedDebugCoStack::default();
+    let mut map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let data = "this is the data";
+
+    stack.push_str(key);
+    expected_stack.push_str(data, "comap_key");
+
+    map.push_key(key.as_bytes(), data.as_bytes()).unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_stack: expected_stack,
+        injected_result_map: map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME_2);
+    initiateAndRun!(harness);
+}
