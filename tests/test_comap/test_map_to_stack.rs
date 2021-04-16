3135b673ed97c98b99e23453b77749c10cb3e87d
dezuman <johannes.borg@hotmail.com>
2021-04-16 17:20:40 +0200

    Add more tests for comap lookup ops

--- tests/test_comap/test_map_to_stack.rs
+++ tests/test_comap/test_map_to_stack.rs
@@ -29,9 +29,126 @@ fn comap_peek() {
     let value = "this is the value";
 
     stack.push_str(key);
-    expected_stack.push_str(value, "comap_key");
+    expected_stack.push_str(value, "comap_value");
 
     map.push_key(key.as_bytes(), value.as_bytes()).unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_stack: expected_stack,
+        injected_result_map: map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME);
+    initiateAndRun!(harness);
+}
+
+#[test]
+#[should_panic]
+fn comap_peek_negtest_wrong_key() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_stack = WrappedDebugCoStack::default();
+    let mut map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let wrong_key = "this is the WRONG key";
+    let value = "this is the value";
+
+    stack.push_str(key);
+    expected_stack.push_str(value, "comap_value");
+
+    map.push_key(wrong_key.as_bytes(), value.as_bytes())
+        .unwrap(); // Push wrong key as contract input
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_stack: expected_stack,
+        injected_result_map: map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME);
+    initiateAndRun!(harness);
+}
+
+#[test]
+#[should_panic]
+fn comap_peek_negtest_wrong_value() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_stack = WrappedDebugCoStack::default();
+    let mut map = DebugCoMap::default();
+
+    let key = "this is the key";
+    let value = "this is the value";
+    let wrong_value = "this is the WRONG value";
+
+    stack.push_str(key);
+    expected_stack.push_str(value, "comap_value");
+
+    map.push_key(key.as_bytes(), wrong_value.as_bytes())
+        .unwrap(); // Push wrong value as contract input
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_stack: expected_stack,
+        injected_result_map: map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME);
+    initiateAndRun!(harness);
+}
+
+#[test]
+fn comap_peek_trucated() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_stack = WrappedDebugCoStack::default();
+    let mut map = DebugCoMap::default();
+
+    let key = "this is the key";
+    
+    // This contract limits size of peeked comap value to 25 bytes
+    let value_fitting = "this is a 25-byte value!!";
+    let value_unfitting = "this is a 25-byte value!! Except for this part!";
+
+    stack.push_str(key);
+    expected_stack.push_str(value_fitting, "comap_value");
+
+    map.push_key(key.as_bytes(), value_unfitting.as_bytes()).unwrap();
+
+    let mut harness = TestHarness::default();
+    harness.debugdata = DebugDataInjector {
+        injected_input_stack: stack,
+        expected_output_stack: expected_stack,
+        injected_result_map: map,
+        ..DebugDataInjector::default()
+    };
+
+    harness.load_contract_binary_default_path(DIR_NAME, CONTRACT_DIR_NAME);
+    initiateAndRun!(harness);
+}
+
+#[test]
+#[should_panic]
+fn comap_peek_trucated_negtest() {
+    let mut stack = DebugCoStack::default();
+    let mut expected_stack = WrappedDebugCoStack::default();
+    let mut map = DebugCoMap::default();
+
+    let key = "this is the key";
+    
+    // This contract limits size of peeked comap value to 25 bytes
+    let _value_fitting = "this is a 25-byte value!!";
+    let value_unfitting = "this is a 25-byte value!! Except for this part!";
+
+    stack.push_str(key);
+    expected_stack.push_str(value_unfitting, "comap_value"); // We expect the un-truncated value, so assertion will fail
+
+    map.push_key(key.as_bytes(), value_unfitting.as_bytes()).unwrap();
 
     let mut harness = TestHarness::default();
     harness.debugdata = DebugDataInjector {
