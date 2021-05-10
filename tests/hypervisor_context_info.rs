ecc4c5c0054f476e336b9d7d9397f3925d74c0f4
dezuman <johannes.borg@hotmail.com>
2021-05-10 16:59:14 +0200

    Add tests for execution_type op
    Contains a small tweak in interface.rs that allows the ExecutionType enum to easily be derived from the raw u32 the hypervisor op returns.

--- tests/hypervisor_context_info.rs
+++ tests/hypervisor_context_info.rs
@@ -22,3 +22,80 @@ fn test_gas_remaining() {
         }
     }
 }
+
+#[test]
+fn test_execution_type_bare_execution() {
+    let result = single_default_execution!("hypervisor_context_info_execution_type");
+
+    // Only 0, 1, and 2 are defined for ExecutionType
+    if result.status > 2 {
+        panic!("\n\nError: u32 returned from function didn't map to a valid ExecutionType!\n\n")
+    }
+
+    let result_execution_type = num::FromPrimitive::from_u32(result.status).unwrap();
+
+    match result_execution_type {
+        ExecutionType::Call => {
+            panic!("\n\nError: Expected ExecutionType 'BareExecution'(2), found 'Call'(0)!\n\n")
+        }
+        ExecutionType::Deploy => {
+            panic!("\n\nError: Expected ExecutionType 'BareExecution'(2), found 'Deploy'(1)!\n\n")
+        }
+        ExecutionType::BareExecution => {} // Correct type!
+    }
+}
+
+#[test]
+fn test_execution_type_deploy() {
+    let mut harness = TestHarness::default();
+    let context = ExecutionContext::create_default_random_context();
+    let result = harness.deploy_binary_using_default_callsystem(
+        &get_contract_path("hypervisor_context_info_execution_type"),
+        context.clone(),
+    );
+
+    // Only 0, 1, and 2 are defined for ExecutionType
+    if result.status > 2 {
+        panic!("\n\nError: u32 returned from function didn't map to a valid ExecutionType!\n\n")
+    }
+
+    let result_execution_type = num::FromPrimitive::from_u32(result.status).unwrap();
+
+    match result_execution_type {
+        ExecutionType::Call => {
+            panic!("\n\nError: Expected ExecutionType 'Deploy'(1), found 'Call'(0)!\n\n")
+        }
+        ExecutionType::Deploy => {} // Correct type!
+        ExecutionType::BareExecution => {
+            panic!("\n\nError: Expected ExecutionType 'Deploy'(1), found 'BareExecution'(2)!\n\n")
+        }
+    }
+}
+
+#[test]
+fn test_execution_type_call() {
+    let mut harness = TestHarness::default();
+    let context = ExecutionContext::create_default_random_context();
+    harness.deploy_binary_using_default_callsystem(
+        &get_contract_path("hypervisor_context_info_execution_type"),
+        context.clone(),
+    );
+    let result = harness.call_using_default_callsystem(context);
+
+    // Only 0, 1, and 2 are defined for ExecutionType
+    if result.status > 2 {
+        panic!("\n\nError: u32 returned from function didn't map to a valid ExecutionType!\n\n")
+    }
+
+    let result_execution_type = num::FromPrimitive::from_u32(result.status).unwrap();
+
+    match result_execution_type {
+        ExecutionType::Call => {} // Correct type!
+        ExecutionType::Deploy => {
+            panic!("\n\nError: Expected ExecutionType 'Call'(0), found 'Deploy'(1)!\n\n")
+        }
+        ExecutionType::BareExecution => {
+            panic!("\n\nError: Expected ExecutionType 'Call'(0), found 'BareExecution'(2)!\n\n")
+        }
+    }
+}
