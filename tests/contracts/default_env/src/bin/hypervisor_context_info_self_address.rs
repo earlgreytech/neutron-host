4984a97f8a492f95ad9136ea9b105060270de650
dezuman <johannes.borg@hotmail.com>
2021-05-10 18:39:40 +0200

    Add tests for self_address, origin, and sender ops

---
+++ tests/contracts/default_env/src/bin/hypervisor_context_info_self_address.rs
@@ -0,0 +1,19 @@
+//! Directly plug a `main` symbol instead of using `#[entry]`
+
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use neutron_star_rt::*;
+extern crate panic_halt;
+
+const DEBUG_DATA_FEATURE: u32 = 0x4000_0001;
+
+#[no_mangle]
+pub unsafe extern "C" fn main() -> ! {
+    __self_address();
+    
+    __system_call(DEBUG_DATA_FEATURE, 2); // DebugDataFunctions::AssertOutputStack
+    
+    __exit(0);
+}
--- tests/hypervisor_context_info.rs
+++ tests/hypervisor_context_info.rs
@@ -1,5 +1,6 @@
 mod common;
 
+use neutron_host::element_interfaces::debug_data::*;
 use neutron_host::harness::*;
 use neutron_host::interface::*;
 
@@ -73,29 +74,60 @@ fn test_execution_type_deploy() {
 }
 
 #[test]
-fn test_execution_type_call() {
+fn test_self_address() {
+    let mut debugdata = DebugDataInjector::default();
     let mut harness = TestHarness::default();
-    let context = ExecutionContext::create_default_random_context();
-    harness.deploy_binary_using_default_callsystem(
-        &get_contract_path("hypervisor_context_info_execution_type"),
-        context.clone(),
+
+    // Create a context with random self address
+    let mut context = ExecutionContext::create_default_random_context();
+
+    debugdata
+        .expect_stack
+        .push_address(context.self_address, "self_address");
+
+    harness.debugdata = debugdata;
+    harness.execute_binary_using_default_callsystem(
+        &get_contract_path("hypervisor_context_info_self_address"),
+        context,
     );
-    let result = harness.call_using_default_callsystem(context);
+}
 
-    // Only 0, 1, and 2 are defined for ExecutionType
-    if result.status > 2 {
-        panic!("\n\nError: u32 returned from function didn't map to a valid ExecutionType!\n\n")
-    }
+#[test]
+fn test_origin() {
+    let mut debugdata = DebugDataInjector::default();
+    let mut harness = TestHarness::default();
 
-    let result_execution_type = num::FromPrimitive::from_u32(result.status).unwrap();
+    // Create a context with random self address and copy it to origin address
+    let mut context = ExecutionContext::create_default_random_context();
+    context.origin = context.self_address;
 
-    match result_execution_type {
-        ExecutionType::Call => {} // Correct type!
-        ExecutionType::Deploy => {
-            panic!("\n\nError: Expected ExecutionType 'Call'(0), found 'Deploy'(1)!\n\n")
-        }
-        ExecutionType::BareExecution => {
-            panic!("\n\nError: Expected ExecutionType 'Call'(0), found 'BareExecution'(2)!\n\n")
-        }
-    }
+    debugdata
+        .expect_stack
+        .push_address(context.origin, "origin");
+
+    harness.debugdata = debugdata;
+    harness.execute_binary_using_default_callsystem(
+        &get_contract_path("hypervisor_context_info_origin"),
+        context,
+    );
+}
+
+#[test]
+fn test_sender() {
+    let mut debugdata = DebugDataInjector::default();
+    let mut harness = TestHarness::default();
+
+    // Create a context with random self address and copy it to sender address
+    let mut context = ExecutionContext::create_default_random_context();
+    context.sender = context.self_address;
+
+    debugdata
+        .expect_stack
+        .push_address(context.sender, "sender");
+
+    harness.debugdata = debugdata;
+    harness.execute_binary_using_default_callsystem(
+        &get_contract_path("hypervisor_context_info_sender"),
+        context,
+    );
 }
