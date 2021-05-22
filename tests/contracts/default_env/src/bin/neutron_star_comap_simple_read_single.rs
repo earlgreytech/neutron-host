08447faf29a4ad8f4f9eeeef0e62b8610f578474
dezuman <johannes.borg@hotmail.com>
2021-05-22 23:11:04 +0200

    Adapt test contract to changed function name

--- tests/contracts/default_env/src/bin/neutron_star_comap_simple_read_single.rs
+++ tests/contracts/default_env/src/bin/neutron_star_comap_simple_read_single.rs
@@ -23,35 +23,35 @@ pub unsafe extern "C" fn main() -> ! {
     
     // NOTE: This was a match statement originally, but a bug was messing that up
     if abi_data == ABI_VALUE_U64 {
-        let value = read_comap_u64(".namespace.keyname").unwrap();
+        let value = read_result_comap_u64(".namespace.keyname").unwrap();
         push_costack_u64(value);
     }
     else if abi_data == ABI_VALUE_U32 {
-        let value = read_comap_u32(".namespace.keyname").unwrap();
+        let value = read_result_comap_u32(".namespace.keyname").unwrap();
         push_costack_u32(value);
     }
     else if abi_data == ABI_VALUE_U16 {
-        let value = read_comap_u16(".namespace.keyname").unwrap();
+        let value = read_result_comap_u16(".namespace.keyname").unwrap();
         push_costack_u16(value);
     }
     else if abi_data == ABI_VALUE_U8 {
-        let value = read_comap_u8(".namespace.keyname").unwrap();
+        let value = read_result_comap_u8(".namespace.keyname").unwrap();
         push_costack_u8(value);
     }
     else if abi_data == ABI_VALUE_I64 {
-        let value = read_comap_i64(".namespace.keyname").unwrap();
+        let value = read_result_comap_i64(".namespace.keyname").unwrap();
         push_costack_i64(value);
     }
     else if abi_data == ABI_VALUE_I32 {
-        let value = read_comap_i32(".namespace.keyname").unwrap();
+        let value = read_result_comap_i32(".namespace.keyname").unwrap();
         push_costack_i32(value);
     }
     else if abi_data == ABI_VALUE_I16 {
-        let value = read_comap_i16(".namespace.keyname").unwrap();
+        let value = read_result_comap_i16(".namespace.keyname").unwrap();
         push_costack_i16(value);
     }
     else if abi_data == ABI_VALUE_I8 {
-        let value = read_comap_i8(".namespace.keyname").unwrap();
+        let value = read_result_comap_i8(".namespace.keyname").unwrap();
         push_costack_i8(value);
     }
     else {
