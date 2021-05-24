deadff89de89e11c70257e7c72fdbfb1879fbfd1
dezuman <johannes.borg@hotmail.com>
2021-05-25 00:05:33 +0200

    Add more explicit error handling to neutron-star testing contracts
    Death to the .unwrap()!

--- tests/contracts/default_env/src/bin/neutron_star_comap_simple_write_array.rs
+++ tests/contracts/default_env/src/bin/neutron_star_comap_simple_write_array.rs
@@ -4,12 +4,18 @@
 #![no_main]
 #![no_std]
 
+use neutron_common::*;
 use neutron_star::syscalls::*;
 use neutron_star::*;
 use neutron_star_rt::*;
 extern crate panic_halt;
 
 const DEBUG_DATA_FEATURE: u32 = 0x4000_0001;
+
+const ERR_STR_ITEMDOESNTEXIST: &str = "ItemDoesntExist (Failed to pop input from costack)";
+const ERR_STR_STACKITEMTOOLARGE: &str = "StackItemTooLarge (The costack input was larger than this function's data type or array size)";
+const ERR_STR_STACKITEMTOOSMALL: &str = "StackItemTooSmall (The costack input was smaller than this function's data type or array size)";
+const ERR_STR_UNHANDLED: &str = "Unhandled";
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
@@ -23,36 +29,76 @@ pub unsafe extern "C" fn main() -> ! {
     // NOTE: This was a match statement originally, but a bug was messing that up
     if abi_data == ABI_VALUE_U8 + ABI_ARRAY_BIT {
         let mut value: [u8; 5] = [0; 5];
-        pop_costack_fixed_array_u8(&mut value).unwrap();
-        write_comap_array_u8(".namespace.keyname", &value);
+        match pop_costack_fixed_array_u8(&mut value) {
+            Ok(()) => write_comap_array_u8(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_u8: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_U16 + ABI_ARRAY_BIT {
         let mut value: [u16; 5] = [0; 5];
-        pop_costack_fixed_array_u16(&mut value).unwrap();
-        write_comap_array_u16(".namespace.keyname", &value);
+        match pop_costack_fixed_array_u16(&mut value) {
+            Ok(()) => write_comap_array_u16(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_u16: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_U32 + ABI_ARRAY_BIT {
         let mut value: [u32; 5] = [0; 5];
-        pop_costack_fixed_array_u32(&mut value).unwrap();
-        write_comap_array_u32(".namespace.keyname", &value);
+        match pop_costack_fixed_array_u32(&mut value) {
+            Ok(()) => write_comap_array_u32(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_u32: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_U64 + ABI_ARRAY_BIT {
         let mut value: [u64; 5] = [0; 5];
-        pop_costack_fixed_array_u64(&mut value).unwrap();
-        write_comap_array_u64(".namespace.keyname", &value);
+        match pop_costack_fixed_array_u64(&mut value) {
+            Ok(()) => write_comap_array_u64(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_u64: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_I8 + ABI_ARRAY_BIT {
         let mut value: [i8; 5] = [0; 5];
-        pop_costack_fixed_array_i8(&mut value).unwrap();
-        write_comap_array_i8(".namespace.keyname", &value);
+        match pop_costack_fixed_array_i8(&mut value) {
+            Ok(()) => write_comap_array_i8(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_i8: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_I16 + ABI_ARRAY_BIT {
         let mut value: [i16; 5] = [0; 5];
-        pop_costack_fixed_array_i16(&mut value).unwrap();
-        write_comap_array_i16(".namespace.keyname", &value);
+        match pop_costack_fixed_array_i16(&mut value) {
+            Ok(()) => write_comap_array_i16(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_i16: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_I32 + ABI_ARRAY_BIT {
         let mut value: [i32; 5] = [0; 5];
-        pop_costack_fixed_array_i32(&mut value).unwrap();
-        write_comap_array_i32(".namespace.keyname", &value);
+        match pop_costack_fixed_array_i32(&mut value) {
+            Ok(()) => write_comap_array_i32(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_i32: {}", ERR_STR_UNHANDLED),
+        }
     } else if abi_data == ABI_VALUE_I64 + ABI_ARRAY_BIT {
         let mut value: [i64; 5] = [0; 5];
-        pop_costack_fixed_array_i64(&mut value).unwrap();
-        write_comap_array_i64(".namespace.keyname", &value);
+        match pop_costack_fixed_array_i64(&mut value) {
+            Ok(()) => write_comap_array_i64(".namespace.keyname", &value),
+            Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_ITEMDOESNTEXIST),
+            Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_STACKITEMTOOLARGE),
+            Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_STACKITEMTOOSMALL),
+            _ => println!("Error in pop_costack_fixed_array_i64: {}", ERR_STR_UNHANDLED),
+        }
     } else {
         println!("Error: Invalid ABI data (This should never happen)");
     }
