e4e53736de0a285d39a78cad3245f297e9d2db10
dezuman <johannes.borg@hotmail.com>
2021-05-25 00:54:26 +0200

    Add explicit error handling for remaining contracts + some test fixes

--- tests/contracts/default_env/src/bin/neutron_star_costack_array_mirror_batch.rs
+++ tests/contracts/default_env/src/bin/neutron_star_costack_array_mirror_batch.rs
@@ -4,12 +4,18 @@
 #![no_main]
 #![no_std]
 
-use neutron_star_rt::*;
-//use neutron_star::*;
+use neutron_common::*;
 use neutron_star::syscalls::*;
+use neutron_star::*;
+use neutron_star_rt::*;
 extern crate panic_halt;
 
 const DEBUG_DATA_FEATURE: u32 = 0x4000_0001;
+
+const ERR_STR_ITEMDOESNTEXIST: &str = "ItemDoesntExist (Failed to pop value from costack)";
+const ERR_STR_STACKITEMTOOLARGE: &str = "StackItemTooLarge (The costack value was larger than this function's data type or array size)";
+const ERR_STR_STACKITEMTOOSMALL: &str = "StackItemTooSmall (The costack value was smaller than this function's data type or array size)";
+const ERR_STR_UNHANDLED: &str = "Unhandled";
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
@@ -19,28 +25,80 @@ pub unsafe extern "C" fn main() -> ! {
     __system_call(DEBUG_DATA_FEATURE, 1); // DebugDataFunctions::PushInputStack
 
     // Initialize arrays that will recieve the popped values before pushing
-    let mut array_u8: [u8; 5] = [0, 0, 0, 0, 0];
-    let mut array_u16: [u16; 5] = [0, 0, 0, 0, 0];
-    let mut array_u32: [u32; 5] = [0, 0, 0, 0, 0];
-    let mut array_u64: [u64; 5] = [0, 0, 0, 0, 0];
-    let mut array_i8: [i8; 5] = [0, 0, 0, 0, 0];
-    let mut array_i16: [i16; 5] = [0, 0, 0, 0, 0];
-    let mut array_i32: [i32; 5] = [0, 0, 0, 0, 0];
-    let mut array_i64: [i64; 5] = [0, 0, 0, 0, 0];
+    let mut array_u8: [u8; 5] = [0; 5];
+    let mut array_u16: [u16; 5] = [0; 5];
+    let mut array_u32: [u32; 5] = [0; 5];
+    let mut array_u64: [u64; 5] = [0; 5];
+    let mut array_i8: [i8; 5] = [0; 5];
+    let mut array_i16: [i16; 5] = [0; 5];
+    let mut array_i32: [i32; 5] = [0; 5];
+    let mut array_i64: [i64; 5] = [0; 5];
+    let mut array_address: [NeutronAddress; 5] = [NeutronAddress::default(); 5];
 
-    //let mut array_address: [i64; 5] = [0,0,0,0,0];
+    match pop_costack_fixed_array_u8(&mut array_u8) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u8(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_u8: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_u16(&mut array_u16) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u16(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_u16: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_u32(&mut array_u32) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u32(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_u32: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_u64(&mut array_u64) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_u64(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_u64: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_i8(&mut array_i8) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i8(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_i8: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_i16(&mut array_i16) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i16(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_i16: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_i32(&mut array_i32) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i32(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_i32: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_i64(&mut array_i64) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_i64(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_i64: {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_fixed_array_address(&mut array_address) {
+        Ok(()) => {}
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_fixed_array_address(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_fixed_array_address(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_fixed_array_address(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_fixed_array_address: {}", ERR_STR_UNHANDLED),
+    }
 
-    // Pop values into array
-    pop_costack_fixed_array_u8(&mut array_u8).unwrap();
-    pop_costack_fixed_array_u16(&mut array_u16).unwrap();
-    pop_costack_fixed_array_u32(&mut array_u32).unwrap();
-    pop_costack_fixed_array_u64(&mut array_u64).unwrap();
-    pop_costack_fixed_array_i8(&mut array_i8).unwrap();
-    pop_costack_fixed_array_i16(&mut array_i16).unwrap();
-    pop_costack_fixed_array_i32(&mut array_i32).unwrap();
-    pop_costack_fixed_array_i64(&mut array_i64).unwrap();
-
-    // Push array values for assertion
     push_costack_array_u8(&array_u8);
     push_costack_array_u16(&array_u16);
     push_costack_array_u32(&array_u32);
@@ -49,6 +107,7 @@ pub unsafe extern "C" fn main() -> ! {
     push_costack_array_i16(&array_i16);
     push_costack_array_i32(&array_i32);
     push_costack_array_i64(&array_i64);
+    push_costack_array_address(&array_address);
 
     // Assert that the state of our output stack matches provided expected state
     __system_call(DEBUG_DATA_FEATURE, 2); // DebugDataFunctions::AssertOutputStack
--- tests/contracts/default_env/src/bin/neutron_star_costack_mirror_batch.rs
+++ tests/contracts/default_env/src/bin/neutron_star_costack_mirror_batch.rs
@@ -4,12 +4,18 @@
 #![no_main]
 #![no_std]
 
-use neutron_star_rt::*;
-//use neutron_star::*;
+use neutron_common::*;
 use neutron_star::syscalls::*;
+use neutron_star::*;
+use neutron_star_rt::*;
 extern crate panic_halt;
 
 const DEBUG_DATA_FEATURE: u32 = 0x4000_0001;
+
+const ERR_STR_ITEMDOESNTEXIST: &str = "ItemDoesntExist (Failed to pop value from costack)";
+const ERR_STR_STACKITEMTOOLARGE: &str = "StackItemTooLarge (The costack value was larger than this function's data type)";
+const ERR_STR_STACKITEMTOOSMALL: &str = "StackItemTooSmall (The costack value was smaller than this function's data type)";
+const ERR_STR_UNHANDLED: &str = "Unhandled";
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
@@ -18,15 +24,69 @@ pub unsafe extern "C" fn main() -> ! {
     __system_call(DEBUG_DATA_FEATURE, 11); // DebugDataFunctions::ReverseInputStack
     __system_call(DEBUG_DATA_FEATURE, 1); // DebugDataFunctions::PushInputStack
 
-    push_costack_u64(pop_costack_u64().unwrap());
-    push_costack_u32(pop_costack_u32().unwrap());
-    push_costack_u16(pop_costack_u16().unwrap());
-    push_costack_u8(pop_costack_u8().unwrap());
-    push_costack_i64(pop_costack_i64().unwrap());
-    push_costack_i32(pop_costack_i32().unwrap());
-    push_costack_i16(pop_costack_i16().unwrap());
-    push_costack_i8(pop_costack_i8().unwrap());
-    push_costack_address(&pop_costack_address().unwrap());
+    match pop_costack_u8() {
+        Ok(v) => push_costack_u8(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_u8(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_u8(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_u8(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_u8(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_u16() {
+        Ok(v) => push_costack_u16(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_u16(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_u16(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_u16(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_u16(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_u32() {
+        Ok(v) => push_costack_u32(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_u32(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_u32(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_u32(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_u32(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_u64() {
+        Ok(v) => push_costack_u64(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_u64(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_u64(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_u64(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_u64(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_i8() {
+        Ok(v) => push_costack_i8(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_i8(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_i8(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_i8(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_i8(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_i16() {
+        Ok(v) => push_costack_i16(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_i16(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_i16(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_i16(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_i16(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_i32() {
+        Ok(v) => push_costack_i32(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_i32(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_i32(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_i32(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_i32(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_i64() {
+        Ok(v) => push_costack_i64(v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_i64(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_i64(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_i64(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_i64(): {}", ERR_STR_UNHANDLED),
+    }
+    match pop_costack_address() {
+        Ok(v) => push_costack_address(&v),
+        Err(RecoverableError::ItemDoesntExist) => println!("Error in pop_costack_address(): {}", ERR_STR_ITEMDOESNTEXIST),
+        Err(RecoverableError::StackItemTooLarge) => println!("Error in pop_costack_address(): {}", ERR_STR_STACKITEMTOOLARGE),
+        Err(RecoverableError::StackItemTooSmall) => println!("Error in pop_costack_address(): {}", ERR_STR_STACKITEMTOOSMALL),
+        _ => println!("Error in pop_costack_address(): {}", ERR_STR_UNHANDLED),
+    }
 
     // Assert that the state of our output stack matches provided expected state
     __system_call(DEBUG_DATA_FEATURE, 2); // DebugDataFunctions::AssertOutputStack
